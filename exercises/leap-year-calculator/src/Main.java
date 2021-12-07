41e4e439bcf08e9ba981ec8dfbb3c0882fff6709
Andy Garcia <acgarzeah@gmail.com>
2021-12-07 10:37:37 -0800

    completed an exercise

---
+++ exercises/leap-year-calculator/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(1700));
+    }
+
+    public static boolean isLeapYear(int year){
+        if (year < 1 || year > 9999) { return false; }
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+}
