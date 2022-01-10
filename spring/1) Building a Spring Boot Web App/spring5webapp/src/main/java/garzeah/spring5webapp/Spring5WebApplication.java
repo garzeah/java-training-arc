d011434eb0b22ec047067aa4e206be5756ab1cfd
Andy Garcia <acgarzeah@gmail.com>
2022-01-09 18:34:27 -0800

    Added new stuff

---
+++ spring/1) Building a Spring Boot Web App/spring5webapp/src/main/java/garzeah/spring5webapp/Spring5WebApplication.java
@@ -0,0 +1,13 @@
+package garzeah.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebApplication.class, args);
+	}
+
+}
---
+++ spring/1) Building a Spring Boot Web App/spring5webapp/src/test/java/garzeah/spring5webapp/Spring5WebApplicationTests.java
@@ -0,0 +1,13 @@
+package garzeah.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
