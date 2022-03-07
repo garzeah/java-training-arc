82682a682972340dc8c7d9d08e61d93d933da54e
Andy Garcia <acgarzeah@gmail.com>
2022-03-06 19:17:38 -0800

    Added some new query dsl logic

--- spring/spring-jpa-hibernate/conference-dl/conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
+++ spring/spring-jpa-hibernate/conference-dl/conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
@@ -3,5 +3,10 @@ package com.pluralsight.conferencedemo.repositories;
 import com.pluralsight.conferencedemo.models.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SessionJpaRepository extends JpaRepository<Session, Long> {
+    List<Session> findBySessionNameContains(String name); // Query DSL returns collection
+    Session findFirstBySessionNameContains(String name); // Query DSL returns first row
+    Long countBySessionNameContains(String name); // Query DSL returns the count
 }
