d9396b992e71efc15b7199a910edaf74f697c8fd
Andy Garcia <acgarzeah@gmail.com>
2022-03-10 10:38:05 -0800

    Finished a Spring Data REST Section

---
+++ spring/spring-rest/conference/conference-demo/src/main/java/com/pluralsight/conferencedemo/models/projections/SessionDetail.java
@@ -0,0 +1,16 @@
+package com.pluralsight.conferencedemo.models.projections;
+
+import com.pluralsight.conferencedemo.models.Session;
+import com.pluralsight.conferencedemo.models.Speaker;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.util.List;
+
+// Projection Example
+@Projection(name="sessionDetail", types = { Session.class })
+public interface SessionDetail {
+    String getSessionName();
+    String getSessionDescription();
+    Integer getSessionLength();
+    List<Speaker> getSpeakers();
+}
---
+++ spring/spring-rest/conference/conference-demo/src/main/java/com/pluralsight/conferencedemo/models/projections/SessionDetailView.java
@@ -0,0 +1,21 @@
+package com.pluralsight.conferencedemo.models.projections;
+
+import com.pluralsight.conferencedemo.models.Session;
+import com.pluralsight.conferencedemo.models.Speaker;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.util.List;
+
+// Virtual Projection
+@Projection(name="sessionDetailView", types = { Session.class })
+public interface SessionDetailView {
+    @Value("#{target.sessionName}")
+    String getTitle();
+    String getSessionDescription();
+    Integer getSessionLength();
+    List<Speaker> getSpeakers();
+
+    @Value("#{target.speakers.size()}")
+    Integer getSpeakerCount();
+}
