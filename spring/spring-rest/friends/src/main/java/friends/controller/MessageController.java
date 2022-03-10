abb5cee1d0c9f63ada483bddbf7e8289fadd8b41
Andy Garcia <acgarzeah@gmail.com>
2022-03-10 14:10:25 -0800

    Finished a section

---
+++ spring/spring-rest/friends/src/main/java/friends/controller/MessageController.java
@@ -0,0 +1,20 @@
+package friends.controller;
+
+import friends.model.Message;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+    @GetMapping("/message")
+    Message send() {
+        return new Message("First message");
+    }
+
+    @PostMapping("/message")
+    Message echo(@RequestBody Message message) {
+        return message;
+    }
+}
