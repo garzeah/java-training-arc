639fb5c98963dcc372f07063e50728ffa24cbb66
Andy Garcia <acgarzeah@gmail.com>
2022-03-10 11:42:49 -0800

    Finished a Spring Data REST Section

---
+++ spring/spring-rest/conference/conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/handlers/SpeakerEventHandler.java
@@ -0,0 +1,29 @@
+package com.pluralsight.conferencedemo.repositories.handlers;
+
+import com.pluralsight.conferencedemo.models.Speaker;
+import com.pluralsight.conferencedemo.repositories.SpeakerJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintViolationException;
+import java.util.HashSet;
+
+@Component
+@RepositoryEventHandler
+public class SpeakerEventHandler {
+    @Autowired
+    private SpeakerJpaRepository repository;
+
+    @HandleBeforeCreate
+    public void handleSpeakerCreate(Speaker s) {
+        // We want to enforce unique speaker last names
+        Speaker foundSpeaker = repository.findFirstByFirstName(s.getFirstName());
+        if(foundSpeaker != null) {
+            System.out.println("First name needs to be unique");
+            throw new ConstraintViolationException("First name needs to be unique", new HashSet<>());
+        }
+
+    }
+}
